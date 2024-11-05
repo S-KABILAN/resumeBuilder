8b62438d93acc55717d5fa1d6dfabf5f6dbf7501
kabilan-s <kabilanselvakumar313@gmail.com>
2024-11-06 00:38:59 +0530

    url update

--- frontend/src/components/GoogleAuth.jsx
+++ frontend/src/components/GoogleAuth.jsx
@@ -4,7 +4,7 @@ const GoogleAuth = () => {
   const handleGoogleLogin = () => {
     // Default to localhost if the environment variable is missing
     const baseUrl = "http://localhost:5000";
-    window.location.href = `${baseUrl}/auth/google`;
+    window.location.href = `resume-builder-ashy-two.vercel.app/auth/google`;
   };
 
   return <button onClick={handleGoogleLogin}>Login with Google</button>;
