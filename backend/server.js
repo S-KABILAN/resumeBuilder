0a24b5dba24f547909158e3e50b890edda1b0f36
kabilan-s <kabilanselvakumar313@gmail.com>
2024-11-06 00:00:46 +0530

    server welcome add

--- backend/server.js
+++ backend/server.js
@@ -45,3 +45,7 @@ app.use("/resume", require("./routes/resumeRoutes"));
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome</h1>");
+});
