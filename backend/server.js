f436fb5128bcf469d6111acabaeba27085e584cc
kabilan-s <kabilanselvakumar313@gmail.com>
2024-11-07 15:55:12 +0530

    server update

--- backend/server.js
+++ backend/server.js
@@ -21,3 +21,8 @@ mongoose
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+app.get("/",(req,res) => {
+  res.send("welcome");
+})
