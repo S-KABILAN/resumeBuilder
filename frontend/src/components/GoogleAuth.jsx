fc1b915ae39ad52ec3e0242339efea37869ade98
kabilan-s <kabilanselvakumar313@gmail.com>
2024-11-09 09:54:02 +0530

    loading

--- frontend/src/components/GoogleAuth.jsx
+++ frontend/src/components/GoogleAuth.jsx
@@ -2,30 +2,31 @@
 import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import { googleLogin } from "../services/api";
 import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
 
-const GoogleAuth = () => {
+const GoogleAuth = ({ onAuthSuccess }) => {
   const navigate = useNavigate();
 
   // src/components/GoogleAuth.jsx
 
-const handleSuccess = async (credentialResponse) => {
-  try {
-    const { credential } = credentialResponse;
-    const response = await googleLogin(credential);
+  const handleSuccess = async (credentialResponse) => {
+    try {
+      const { credential } = credentialResponse;
+      const response = await googleLogin(credential);
 
-    if (response.success) {
-      console.log("Login successful:", response.data);
-      // Save token and user details to local storage
-      localStorage.setItem("token", response.data.token);
-      localStorage.setItem("user", JSON.stringify(response.data.user)); // Store user details
-      // Redirect to dashboard
-      navigate("/dashboard");
+      if (response.success) {
+        console.log("Login successful:", response.data);
+        // Save token and user details to local storage
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("user", JSON.stringify(response.data.user)); // Store user details
+        // Redirect to dashboard
+        onAuthSuccess();
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      console.error("Login failed:", error.message);
     }
-  } catch (error) {
-    console.error("Login failed:", error.message);
-  }
-};
-
+  };
 
   const handleError = () => {
     console.error("Google Login failed");
@@ -37,5 +38,10 @@ const handleSuccess = async (credentialResponse) => {
     </GoogleOAuthProvider>
   );
 };
+
+GoogleAuth.propTypes = {
+  onAuthSuccess: PropTypes.func.isRequired,
+};
+
 
 export default GoogleAuth;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,28 @@
+// src/pages/Login.jsx
 import GoogleAuth from "../components/GoogleAuth";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setLoading(true);
+      navigate("/dashboard");
+    }
+  }, [navigate]);
+
   return (
     <div>
       <h2 className="text-red-500 text-center text-4xl">Login with Google</h2>
-      <GoogleAuth />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <GoogleAuth onAuthSuccess={() => navigate("/dashboard")} />
+      )}
     </div>
   );
 }
